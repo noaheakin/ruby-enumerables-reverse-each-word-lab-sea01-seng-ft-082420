597aeecf14e3d718b92acdff12721d019f3f259c
Noah Eakin <noaheakin@gmail.com>
2020-08-10 21:04:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word (string)
   new_array = []
   modify = string.split
-  modify.each do |element|
+  modify.collect do |element|
     new_array << element.reverse
   end
   new_array.join (" ")
